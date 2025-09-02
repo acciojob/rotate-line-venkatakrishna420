a69772cd5679df28e9c1de6db18ebcdff822a917
KAKARLA VENKATA KRISHNA <kakarlavenkatakrishna9@gmail.com>
2025-09-02 14:18:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 //your JS code here. If required.
 const line=document.getElementById("line");
-	setIntervel(()=>{line.style.transform=rotate(2deg)},20)
+let angle=2;
+	setIntervel(()=>{
+		angle+=2
+		line.style.transform=`rotate(${angle}deg)`},20)
 	

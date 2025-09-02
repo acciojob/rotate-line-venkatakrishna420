05a59e2bca50597efce5f6c52c20233870c37318
KAKARLA VENKATA KRISHNA <kakarlavenkatakrishna9@gmail.com>
2025-09-02 14:32:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-//your JS code here. If required.
-const line=document.getElementById("line");
-let angle=2;
-	setIntervel(()=>{
-		angle+=2
-		line.style.transform=` translate(-50%, -50%) rotate(${angle}deg)`},20)
-	
+let angle = 0; 
+const line = document.getElementById("line");
+
+// Correct spelling: setInterval (not setIntervel)
+setInterval(() => {
+  angle += 2; 
+  line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`; 
+}, 20);
